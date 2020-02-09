78c81ebe14f271443a0f59db0634be93b5ac8e30
FacuAraujo <47833388+FacuAraujo@users.noreply.github.com>
2020-02-09 15:35:15 -0300

    build commit

---
+++ precache-manifest.0dad85e706798f90fae8218c3dffab3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34031569b1509fb2670e9c50cf3e543d",
+    "url": "/logoquiz-esports-game/index.html"
+  },
+  {
+    "revision": "27c64132f018627cdedf",
+    "url": "/logoquiz-esports-game/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "4cdd3f685748e9dd6204",
+    "url": "/logoquiz-esports-game/static/css/main.e123e2e6.chunk.css"
+  },
+  {
+    "revision": "27c64132f018627cdedf",
+    "url": "/logoquiz-esports-game/static/js/2.86c87313.chunk.js"
+  },
+  {
+    "revision": "4cdd3f685748e9dd6204",
+    "url": "/logoquiz-esports-game/static/js/main.86c00f8e.chunk.js"
+  },
+  {
+    "revision": "420f41a847ff9c9a0218",
+    "url": "/logoquiz-esports-game/static/js/runtime-main.c86da41f.js"
+  },
+  {
+    "revision": "99419477bf93fcfe574049e8d7dfeac2",
+    "url": "/logoquiz-esports-game/static/media/LogoQuiz_logo.99419477.svg"
+  }
+]);
